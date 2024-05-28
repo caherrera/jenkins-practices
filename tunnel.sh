9675e17d1ed4c3a20fec19819abcff3b2fc44ac2
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2024-05-28 13:57:29 -0400

    cambios varios

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-ssh vagrant@localhost -p2222 -i .vagrant/machines/default/virtualbox/private_key -L 18080:localhost:8080 -Nnv -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no
+ssh vagrant@localhost -p2222 -i .vagrant/machines/default/virtualbox/private_key -L 8080:localhost:8080 -Nnv -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no
  
